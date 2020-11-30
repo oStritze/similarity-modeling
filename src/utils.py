5d25b81498d1c1af918cb8f3d5f864425aa7f3e0
oStritze <oliver.stritzel@googlemail.com>
2020-11-30 10:31:32 +0100

    added some python files for easier usage in notebook

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,13 @@
 import pandas as pd
 import numpy as np
 import os.path
+import visual_feat_ext
+
+def check_movie_files():
+    print("checking for raw movies.mp4 files...")
+    files = ["Muppets-02-01-01.avi", "Muppets-02-04-04.avi", "Muppets-03-04-03.avi"]
+    for f in files:
+        print("../media/"+f)
+        os.path.isfile("../media/"+f)
+    print("all present!")
+
